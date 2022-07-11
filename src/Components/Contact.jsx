aa7495c8c0d0cf99a9b624227b34eeaa0b1e20ef
carlafaes <carlafaes18@hotmail.com>
2022-07-11 19:38:56 -0300

    form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,10 @@
+import Mailer from "./Mailer"
+
 export default function Contact() {
     return(
         <div style={{height:'100vh'}}>
             <h1>Contacto</h1>
+            <Mailer/>
         </div>
     )
 }
---
+++ src/Components/Mailer.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Grid from '@mui/material/Grid';
+
+
+
+//probando terminal
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Row } from 'react-bootstrap';
+
+
+export default function Mailer(initial = {}) {
+    const [errors, setErrors] = useState({}) //estado local que arranca con un obj vacio
+    const [inputForm, setInputForm] = useState({
+        user_name: '',
+        user_email: '',
+        user_message: ''
+    })
+
+
+
+
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_stu3969', 'template_lejafpj', e.target, 'H0NguBXqHykea033Q')
+            .then(response => {
+                message();
+                setInputForm({
+                    user_name: '',
+                    user_email: '',
+                    user_message: ''
+                })
+            })
+            .catch(error => messageError())
+    }
+
+    const message = () => {
+        toast.success('Message send succesfully!', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
+
+    const messageError = () => {
+        toast.error('Ups!something is wrong, please try again later', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
+
+
+
+    return (
+        <div >
+            <Form onSubmit={sendEmail}>
+                <Form.Group  className="mb-4" controlId="formBasicEmail">
+                    <InputGroup className="mb-3" name='user_name'>
+                    <InputGroup.Text>Nombre</InputGroup.Text>
+                        <Form.Control
+                            placeholder="Ingresa tu nombre"
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup >
+                    <InputGroup className="mb-3">
+                    <InputGroup.Text>Email</InputGroup.Text>
+                    <Form.Control type="email" placeholder="Ingresa tu email" name='user_email'/>
+                    
+                    <Form.Text className="text-muted">
+                        No se compartira tu email con nadie.
+                    </Form.Text>
+                    </InputGroup>
+
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>Tu mensaje</InputGroup.Text>
+                    <Form.Control as="textarea" aria-label="With textarea" name='user_message'/>
+                </InputGroup>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Enviar
+                </Button>
+            </Form>
+
+            <ToastContainer
+                theme='dark'
+            />
+
+        </div>
+    )
+}
